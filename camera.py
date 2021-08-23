c4d011ce435ff3a7a60f15fce590461d7582e836
hacknus <l_stoeckli@bluewin.ch>
2021-08-23 13:52:35 +0200

    updated coca parameters

--- camera.py
+++ camera.py
@@ -4,12 +4,12 @@ import numpy as np
 class Camera:
 
     def __init__(self, r_h=1, t_exp=0.001):
-        self.G = 2.68  # electron per DN
+        self.G = 3.5  # electron per DN
         self.d_pixel = 7e-6  # meters
         self.A_pixel = self.d_pixel ** 2
         self.d_aperture = 0.135  # meters
         self.A_aperture = (self.d_aperture / 2) ** 2 * np.pi
-        self.focal_length = 0.875  # meters
+        self.focal_length = 0.880  # meters
         self.Omega = 0.64e-10
         self.A_Omega = self.A_aperture * self.A_pixel / self.focal_length ** 2
         self.r_h = r_h  # A.U.
